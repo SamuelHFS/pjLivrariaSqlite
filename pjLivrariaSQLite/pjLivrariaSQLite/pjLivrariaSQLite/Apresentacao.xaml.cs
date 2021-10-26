12b70718f949ca71d4949228bd462c5473e3fed3
SamuelHFS <samuel.jpg1@gmail.com>
2021-10-26 11:56:17 -0300

    falta terminar

---
+++ pjLivrariaSQLite/pjLivrariaSQLite/pjLivrariaSQLite/Apresentacao.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace pjLivrariaSQLite
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Apresentacao : ContentPage
+    {
+        public Apresentacao()
+        {
+            InitializeComponent();
+
+            //Criar pagina apresentacao
+//lb1.Text += "\nCódigo: " + Application.Current.Properties["Codigo"].ToString();
+        }
+    }
+}
